7cf0bd4022a1e149d056be7232b6a5c9594c993a
korachof <korachof@gmail.com>
2025-07-28 10:04:17 -0700

    Created a settings.json file to make sure all tabs are set to 4 spaces, and that spaces are used.
    Added the create or alter procedure to ThreeDModelTrackerDB, as well as the validation for the NOT NULL parameters.
    
    Added validation that trims leading and trailing spaces, and checks if the model name is empty. If so, it raises an error and returns -1.
    
    For Source URL, if that is empty, raises an error, and it returns -2.

--- procedures/add_model.sql
+++ procedures/add_model.sql
@@ -1,3 +1,6 @@
+USE ThreeDModelTrackerDB;
+GO
+
 -- =============================================
 -- Procedure Name: add_model
 -- Purpose: Inserts a new 3D model into the Models table
@@ -6,6 +9,35 @@
 --   @name              NVARCHAR(255)     -- Display name of the model
 --   @source_url        NVARCHAR(MAX)     -- Link to model source (e.g., Thingiverse)
 --   @license_type      NVARCHAR(100)     -- License classification (e.g., Creative Commons)
---   @description       NVARCHAR(MAX)     -- Optional model notes or summary
+--   @model_description       NVARCHAR(MAX)     -- Optional model notes or summary
 -- Returns: Newly inserted Model ID (INT)
 -- =============================================
+
+CREATE OR ALTER PROCEDURE AddModel
+    @ModelName NVARCHAR(255),
+    @SourceURL NVARCHAR(MAX),
+    @LicenseType NVARCHAR(100) = NULL, -- Optional parameter
+    @ModelDescription NVARCHAR(MAX) = NULL -- Optional parameter
+
+AS
+BEGIN
+
+/* Prevent unnecessary output and aid in performance
+   (Supress default back messages) */ 
+    SET NOCOUNT ON;
+
+    -- Validation/Error Handling for NOT NULL columns
+
+    -- LTRIM(RTRIM) first removes all trailing spaces, then all leading spaces
+    IF @ModelName IS NULL OR LTRIM(RTRIM(@ModelName)) = ''
+        BEGIN
+            RAISERROR('Model name cannot be empty.', 16, 1);
+            RETURN -1; -- Set -1 for naming issue
+        END
+
+    IF @SourceURL IS NULL OR LTRIM(RTRIM(@SourceURL)) = ''
+    BEGIN
+        RAISERROR('Source URL cannot be empty.', 16, 1);
+        RETURN -2;
+    END
+
