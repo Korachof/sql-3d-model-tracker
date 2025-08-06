6b879c2e0d06248a6e0d8a1bb7c9f93dfa9741b1
korachof <korachof@gmail.com>
2025-08-06 09:23:01 -0700

    Created a simple Get Printers read procedure that returns a result set containing all columns for all printers. Follows the same pattern as Get Models.

---
+++ procedures/GetPrinters.sql
@@ -0,0 +1,32 @@
+-- =============================================
+-- Author:      Chris Partin
+-- Create date: 2025-08-06
+-- Description: Retrieves a list of all printers from the dbo.Printers table.
+--
+-- Parameters:
+--   None
+--
+-- Returns:
+--   A result set containing all columns for all printers.
+--
+-- =============================================
+CREATE OR ALTER PROCEDURE dbo.GetPrinters
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Selects all data from the Printers table.
+    -- The results are ordered by Brand, then by Model Name for a predictable sort.
+    SELECT
+        PrinterID,
+        PrinterBrand,
+        PrinterModelName,
+        PrinterMaterialType
+    FROM
+        dbo.Printers
+    ORDER BY
+        PrinterBrand ASC,
+        PrinterModelName ASC;
+
+END
+GO
