66e34c6892f6d0537570bc4a5edc2f59d30cfe7d
korachof <korachof@gmail.com>
2025-08-06 09:29:35 -0700

    Created a simple tester for GetPrinters.sql. It follows similar logic with GetModels.
    As usual, my testing files need to be cleaned up for consistency and edge cases, but for now, it's working as intended! tested in SSMS and working well.

---
+++ tests/TestGetPrinters.sql
@@ -0,0 +1,30 @@
+-- =============================================
+-- Test Script for dbo.GetPrinters
+-- =============================================
+-- First, ensure we have a clean slate.
+-- We must delete from PrintLog first due to the foreign key constraint.
+DELETE FROM dbo.PrintLog;
+DELETE FROM dbo.Printers;
+DBCC CHECKIDENT ('dbo.Printers', RESEED, 0);
+GO
+
+-- Declare variables for the entire test batch
+DECLARE @PrinterID_1 INT, @PrinterID_2 INT, @PrinterID_3 INT;
+DECLARE @ReturnStatus INT;
+
+-- Insert a few sample printers out of alphabetical order
+PRINT '--- Inserting test printers... ---';
+EXEC @ReturnStatus = dbo.AddPrinter @PrinterBrand = N'Elegoo', @PrinterModelName = N'Mars 4 Ultra', @PrinterMaterialType = N'Resin', @PrinterID = @PrinterID_1 OUTPUT;
+EXEC @ReturnStatus = dbo.AddPrinter @PrinterBrand = N'Prusa', @PrinterModelName = N'MK4', @PrinterMaterialType = N'Filament', @PrinterID = @PrinterID_2 OUTPUT;
+EXEC @ReturnStatus = dbo.AddPrinter @PrinterBrand = N'Bambu Lab', @PrinterModelName = N'P1S', @PrinterMaterialType = N'Filament', @PrinterID = @PrinterID_3 OUTPUT;
+
+-- Verify that 3 printers were added to the table
+SELECT * FROM dbo.Printers;
+GO
+
+---
+PRINT '--- EXECUTING dbo.GetPrinters ---';
+-- Expected: A result set with the 3 printers listed above, sorted alphabetically
+-- first by PrinterBrand, then by PrinterModelName.
+EXEC dbo.GetPrinters;
+GO
